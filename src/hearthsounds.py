6807a8f02e73b9f3e1d3bc074634417b5f699153
Max Timkovich <mtimkovich@gmail.com>
2016-11-01 13:07:58 -0700

    raw sql

--- src/hearthsounds.py
+++ src/hearthsounds.py
@@ -2,9 +2,10 @@
 import cgi
 import json
 from mako.template import Template
-import peewee as pw
+import MySQLdb
 import re
 import requests
+import yaml
 
 print "Content-Type: text/html"
 print
@@ -12,16 +13,35 @@ print
 def verify_url(url):
     return re.match(r'(http://)?(www\.)?hearthhead\.com', url)
 
-form = cgi.FieldStorage()
-url = form.getvalue('url', '')
+def get_card_id(url):
+    m = re.search('/cards/([^/]*)', url)
+    return m.group(1)
 
 class Card:
-    def __init__(self, j):
+    def __init__(self, card_id, db):
         self.BASE_URL = 'http://media.services.zam.com/v1/media/byName'
+        self.card_id = card_id
+        self.db = db
+        self.cursor = db.cursor(MySQLdb.cursors.DictCursor)
+
+    def from_json(self, j):
         self.name = j['name']
         self.image = self.BASE_URL + next(i['url'] for i in j['media'] if i['type'] == 'CARD_IMAGE')
         sound_json = [i for i in j['media'] if 'SOUND' in i['type']]
         self.sounds = self.get_sounds(sound_json)
+
+    def from_sql(self):
+        self.cursor.execute('select * from cards where card_id = %s limit 1', (self.card_id,))
+        row = self.cursor.fetchone()
+
+        if row:
+            self.card_id = row['card_id']
+            self.name = row['name']
+            self.image = row['image']
+            self.sounds = json.loads(row['sounds'])
+            return True
+
+        return False
 
     def get_sounds(self, sound_json):
         sounds = []
@@ -32,30 +52,27 @@ class Card:
 
         return sounds
 
-db = pw.MySQLDatabase('hearthstone', user='finley')
+    def insert(self):
+        self.cursor.execute('insert into cards (card_id, name, image, sounds) values (%s, %s, %s, %s)',
+                            (self.card_id, self.name, self.image, json.dumps(self.sounds)))
+        self.db.commit()
 
-class CardModel(pw.Model):
-    url = pw.CharField(unique=True)
-    name = pw.CharField()
-    image = pw.CharField()
-    sounds = pw.TextField()
+form = cgi.FieldStorage()
+url = form.getvalue('url', '')
 
-    class Meta:
-        database = db
+config = yaml.load(open('/home/max/Python/db.yaml'))
+db = MySQLdb.connect(**config)
 
 card_name = ''
 if url and verify_url(url):
     if not url.startswith('http://'):
         url = 'http://' + url
 
-    db.connect()
+    card_id = get_card_id(url)
+    card = Card(card_id, db)
+    exists = card.from_sql()
 
-    try:
-        model = CardModel.get(CardModel.url == url)
-    except pw.DoesNotExist:
-        model = None
-
-    if not model:
+    if not exists:
         r = requests.get(url)
         html = r.text.encode('utf-8')
 
@@ -63,18 +80,15 @@ if url and verify_url(url):
             if 'card:' in line:
                 line = line.replace('card:', '', 1)
                 j = json.loads(line)
-                card = Card(j)
+                card.from_json(j)
+                card.insert()
 
-                model = CardModel(url=url,
-                                  name=card.name,
-                                  image=card.image, 
-                                  sounds=json.dumps(card.sounds))
-
-                model.save()
                 break
-        db.close()
+
 else:
     card = None
+
+db.close()
 
 
 error = ''
@@ -82,6 +96,5 @@ if url and not verify_url(url):
     error = 'Invalid url provided'
 
 print Template(filename='template.html').render(url=cgi.escape(url, True), 
-                                card=model,
-                                sounds=json.loads(model.sounds),
-                                error=error)
+                                                card=card,
+                                                error=error)
